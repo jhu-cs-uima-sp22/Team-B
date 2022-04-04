98196bc9eb27767ebed2147e8e23e74d81f482af
Elijah Eaton <eaton.elijah@gmail.com>
2022-04-04 17:12:03 -0400

    continued work on main view

---
+++ app/src/main/java/com/example/disco/SingleSongRow.java
@@ -0,0 +1,13 @@
+package com.example.disco;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SingleSongRow extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.single_song_row);
+    }
+}
